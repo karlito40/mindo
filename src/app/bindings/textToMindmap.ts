43a928851c2e030aecd7230ca37a7d8b854d2c1a
karlito40 <karlito40@gmail.com>
2025-04-30 13:05:42 +0200

    package

--- src/app/bindings/textToMindmap.ts
+++ src/app/bindings/textToMindmap.ts
@@ -1,10 +1,10 @@
 import * as vscode from "vscode";
-import { debounce } from "lodash-es";
+import { debounce } from "lodash";
 import { drawMindmap, findMindoTextEditor } from "../../mindo";
 
 export function textToMindmap({ subscriptions }: vscode.ExtensionContext) {
   // prettier-ignore
-  vscode.workspace.onDidChangeTextDocument(debounce((event) => {
+  vscode.workspace.onDidChangeTextDocument(debounce((event: any) => {
     renderTasks(event.document);
   }, 300), null, subscriptions);
 }
